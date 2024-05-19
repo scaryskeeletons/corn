67108b7d157d29f25c9165fca53f6ced1af963c7
unknown <admin@zFu9crAQU7.zFu9crAQU7>
2024-05-18 19:11:06 -0700

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   plugins: [
-    sveltekit(),
-    inject({
-      Buffer: ['buffer', 'Buffer'],
-    }),
+    sveltekit()
   ],
 });
