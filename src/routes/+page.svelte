57a3b1b6987b60707604ce84af4606ff558275b9
unknown <admin@zFu9crAQU7.zFu9crAQU7>
2024-05-21 18:44:42 -0700

    gay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,73 +1,44 @@
-<script>
-  import { onMount } from 'svelte';
+<!-- Animation is defined via SVG -->
 
-  let animation_length = 5; // Speed at which kernels fall
-
-  onMount(() => {
-      const container = document.querySelector('#pop_corn_container');
-
-      function createPopcorn() {
-          var p = document.createElement('div');
-          p.className = 'dot';
-          // Pick random size between 25-60
-          var scale = (Math.random() * 35) + 25;
-          p.style.width = `${scale}px`;
-          p.style.height = `${scale}px`;
-          p.style.left = `${Math.random() * (window.innerWidth - 35)}px`;
-          p.style.top = `${Math.random() * (window.innerHeight * 0.5)}px`;
-          p.style.animation = `pop ${animation_length}s linear forwards`;
-          container.appendChild(p);
-
-          // Remove excess kernels
-          setTimeout(() => {
-              p.remove();
-          }, 1000 * animation_length);
-      }
-
-      // Create a kernel 800ms
-      const intervalId = setInterval(createPopcorn, 800);
-
-      // Clean up the interval when the component is destroyed
-      return () => {
-          clearInterval(intervalId);
-      };
-  });
-</script>
-
-<div class="section">
-  <!-- First Section -->
-  <div class="social-links">
-      <a href="https://x.com/sprinklesrip" target="_blank" rel="noopener" class="social-link">
-          <img src="https://github.com/scaryskeeletons/images/raw/main/twitter.webp" alt="Twitter"/>
-      </a>
-      <a href="https://www.pump.fun/BaqoQxuZGFVvHTz94Bwae3VHEpv7VdsVUXb4xmoRWoUV" target="_blank" rel="noopener" class="social-link">
-          <img src="https://github.com/scaryskeeletons/images/raw/main/dexscreener.webp" alt="Dex Screener"/>
-      </a>
-      <a href="https://t.me/sprinklesrip" target="_blank" rel="noopener" class="social-link">
-          <img src="https://github.com/scaryskeeletons/images/raw/main/telegram.webp" alt="Telegram"/>
-      </a>
-  </div>
-
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/PnJo7d-xBjY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+<svg viewBox="0 0 0 0" fill="none" class="size-0">
+    <defs>
+      <filter id="filter">
+        <feTurbulence id="turbulence-filter" type="fractalNoise" baseFrequency=".03" numOctaves="1" result="noise" />
+        <feDisplacementMap in="SourceGraphic" in2="noise" scale="20" xChannelSelector="B" yChannelSelector="G" />
+      </filter>
+      
+      <animate
+               id="anim1"
+               xlink:href="#turbulence-filter"
+               attributeName="baseFrequency"
+               from=".03"
+               to=".033"
+               dur="3s"
+               begin="0s; anim2.end"
+               fill="freeze"/>
+      <animate
+               id="anim2"
+               xlink:href="#turbulence-filter"
+               attributeName="baseFrequency"
+               from=".033"
+               to=".03"
+               dur="3s"
+               begin="anim1.end"
+               fill="freeze"
+               />
   
-  <div id="pop_corn_container"></div>
-</div>
-
-<!-- Story Section -->
-<div class="section story">
-  <h1>The story of Sprinkles</h1>
-  <h2>YouTube's Popcorn Cat</h2>
-  <p>
-      Sprinkles, a cat famous for the YouTube video "Cat terrified by popcorn" was a cat who was quite quirky and fun.
-  </p>
-  <p>
-      Unfortunately one day Sprinkles went to the doctor and while getting a teeth cleaning Sprinkles tragically passed away from anesthesia at just two years old.
-  </p>
-  <p>
-      Today $SPRINKLES represents a celebration of life and provides a way for the story of Sprinkles to live forever in our hearts.
-  </p>
-</div>
-
-<style>
-  @import './page/page.css';
-</style>
+      </defs>
+  </svg>
+  
+  <!-- Demo (with Tailwind CSS) -->
+  
+  <header class="h-screen overflow-hidden">
+    <img src='https://images.unsplash.com/photo-1492931307820-62fa5a68e0df?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTU2Njk3MDN8&ixlib=rb-4.0.3&q=85' alt='' class="object-cover scale-105" style="filter: url(#filter);">
+  </header>
+  
+  <main class="container mx-auto my-24">
+    <h2 class="text-4xl mb-8 text-center">Demo on a simple <code>&lt;div&gt;</code>-Element</h2>
+    <div class="w-1/3 aspect-square bg-gradient-to-r from-cyan-500 to-blue-500 mx-auto" style="filter: url(#filter);"></div>
+  </main>
+  
+  
